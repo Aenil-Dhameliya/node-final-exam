ed44f045210d11738313f38b7a838a4d8f8343e4
Aenil-Dhameliya <124959880+Aenil-Dhameliya@users.noreply.github.com>
2023-10-12 08:53:08 +0530

    project done

---
+++ src/controllers/index.js
@@ -0,0 +1,4 @@
+module.exports.userController = require("./user.controller");
+module.exports.menuController = require("./menu.controller");
+module.exports.orderController = require("./order.controller");
+module.exports.reviewController = require("./review.controller");
